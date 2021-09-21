18bdeffbbd01d22a4af169d00add19f9d3f407b9
Matheus Marchione <matheusmd@ciandt.com>
2021-09-21 11:31:07 -0300

    add mutation test

---
+++ stryker.conf.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  testRunner: "jest",
+  reporters: ["progress", "clear-text", "html"],
+  coverageAnalysis: "off",
+  jest: {
+      config: require('./jest.config'),
+  },
+  mutate: ["spec/**/*.test.{js,jsx}"]
+};
